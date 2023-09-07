31720e252cc4a7fb3f77282c813ebf5b2db87d67
RepoAng <144359275+RepoAng@users.noreply.github.com>
2023-09-07 10:51:59 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import {of, from } from 'rxjs';
 
@@ -9,5 +10,17 @@ export class AppComponent implements OnInit  {
 
   ngOnInit() {
     of(2,3,4,5).subscribe(item => console.log(item));
-  }
+
+    from([20,30,40,50]).subscribe({
+      next: (item) =>console.log(item),
+      error: (ex) => console.log(ex),
+      complete:() => console.log('done')
+     } );
+
+     of ('Apple1', 'Apple2','Apple3').subscribe({
+      next:(item) => console.log(item)
+      error: (ex) => console.log(ex),
+      complete:() => console.log('done')
+     });
+  }  
 }
